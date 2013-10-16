85b4852a7385b22828da7b6562db831efddf294a
azyner <azyner@gmail.com>
2013-10-16 12:23:54 +1100

    Modified parser to interpret lists down to instruction level

--- intermediate_code_parser.py
+++ intermediate_code_parser.py
@@ -5,6 +5,24 @@
 
 from pyparsing import OneOrMore, nestedExpr
 
-all_the_text = open('thefile.txt').read()      # all text from a text file
+all_the_text = open('tests/simple.in').read()      # all text from a text file
 data = OneOrMore(nestedExpr()).parseString(all_the_text)
-print data
+data = data[0];                                    # Remove redundant first list layer
+print 'Data is length: '
+print len(data)
+for i in range(0,len(data)):
+    #print 'Function: ' + repr(i) + repr(data[i])
+    #This is the function layer
+    #data[i][0] is the function name
+    #data[i][1] is the args
+    #data[i][2-len] is the blocks
+    print 'Function name is: ' +repr(data[i][0])
+    print 'Arguments are: ' + repr(data[i][1])
+    for j in range(2,len(data[i])):
+        print 'Blocks number is: ' 
+        print data[i][j][0]
+        print 'Instructions list: '
+        for k in range(1,len(data[i][j])):
+            print data[i][j][k]
+
+#print data
