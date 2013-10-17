6915dea776cf51bc41a832c2b251e63da21e3e34
Christopher Rosiak <cr5210@gmail.com>
2013-10-17 15:52:13 +1100

    Created another file for task 3

---
+++ dead_code_elimination.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+## Python module for task 3 of the COMP3109 Assignment 3.
+## This module performs dead code elimintation.
+## Author: Christopher Rosiak
+## Date: 2013-10-17
+
+
