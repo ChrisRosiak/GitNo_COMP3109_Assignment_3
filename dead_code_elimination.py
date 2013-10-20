6ad95fe5645841689c954c9f6bf691e1ce1e4d97
Christopher Rosiak <cr5210@gmail.com>
2013-10-20 19:14:01 +1100

    Update to the documentation.

--- dead_code_elimination.py
+++ dead_code_elimination.py
@@ -6,3 +6,15 @@
 ## Date: 2013-10-17
 
 
+# Pseudocode of the iterator algorithm.
+
+#for each node n in CFG                 # Initialise solutions
+#   in[n] = None ; out[n] = None        ##
+   
+#repeat
+#   for each node n in CFG in reverse topsort order
+#     in'[n] = in[n]                                        # Save current results
+#     out'[n] = out[n]                                      ##
+#     out[n] = Union in[s] (where s is an element from the set of all successors of the node n)    # Solve data-flow equations
+#     in[n] = use[n] Union (out[n] - def[n])                                                       ##
+#until in'[n] = in[n] and out'[n] = out[n] for all n               ## Test for convergence
