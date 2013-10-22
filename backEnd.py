72f2de1f7ffe8e82b5cc95ddbaa4593c4809c3cf
azyner <azyner@gmail.com>
2013-10-22 14:43:45 +1100

    Added a instruction list for each block node. Remove debug prints as desired. The empty set at the end of lists is a print error, not an actual artifact. Adjusted comments.

--- backEnd.py
+++ backEnd.py
@@ -16,13 +16,11 @@ class blockNode:
     def __init__(self,blockNumber):
         self.inSet = []
         self.outSet = []
-        #children is a STRING list of block names.
-        #A dictionary will be required to match names with block reference
-        #This makes tree construction simpler, as it solves the problem of
-        #trying to refernce a child node that has not been constructed yet.
+        #children starts as a string list of node References. It is then fixed to actual references, otherwise it attempts to link a node that has not been created yet.
         self.children = []
         self.readReg = []
         self.writeReg = []
+        self.instrs = [] #list of instructions for block
         self.name = blockNumber
         self.visited = False
 
@@ -80,20 +78,23 @@ for i in range(0,len(data)):
             #Scan for register reads, writes and branches here
             #Register read/writes will be useful for dead code detection
             #Branch detection will be needed for unreachable code analysis
+
+            #But first, add the instruction to the node instruction list
+            currentNode.instrs.append(data[i][j][k])
+
             if(data[i][j][k][0] == 'br'):
                 currentNode.children.append(data[i][j][k][2])
                 currentNode.children.append(data[i][j][k][3])
 
             if(data[i][j][k][0] == 'call'):
-                #this is where the nex function would be added to the work queue
+                #If non-called functions are to be culled, this is where functions should be marked as called, and therefore the functions are reachable.
                 print data[i][j][k][2]
                 print 'Called'
 
             print data[i][j][k]
 
     functionList.append(functionTree(data[i][0],data[i][1],functionBlocks['0']))
-    # It might be preferable to go around and fix the children references at this
-    # point in code.
+    #This is where the string references to blocks are fixed to be actual node references using the functionBlocks dictionary.
     for blkName, blkRef in functionBlocks.iteritems():
         #read in child list
         stringList = blkRef.children
@@ -103,13 +104,9 @@ for i in range(0,len(data)):
         for child in stringList:
             blkRef.children.append(functionBlocks[child])
 
-
 ##TREE CONSTRUCTION FINISHED
 
-#START TASK 2 BFS HERE
-
-
-
+##START TASK 2 DFS HERE
 def bfsTree(block,functionTree):
     block.visited = True
     #add block REFERENCE to the function visited list.
@@ -121,13 +118,11 @@ def bfsTree(block,functionTree):
 
 
 for func in functionList:
-    #This should not be a for loop, instead the traverser should start at 'main'
-    #and add any functions that are called to a work list. I (Alex) am starting this
-    #way because I'm tackling the block node traversal problem first.
     bfsTree(func.headBlock,func)
     print 'Function Name: '
     print func.name
     print 'Visited Blocks: '
     for blk in func.visitedBlocks:
         print blk.name
+        print blk.instrs
 
